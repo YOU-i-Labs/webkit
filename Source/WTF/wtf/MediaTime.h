a29a4489d370a15d583b7d2ae47c77aa8127089d
Patrick White <patrick.white@youi.tv>
2020-05-19 11:51:15 -0400

    C++17 fixes for linux / bluesky

--- Source/WTF/wtf/MediaTime.h
+++ Source/WTF/wtf/MediaTime.h
@@ -53,8 +53,8 @@ public:
         DoubleValue = 1 << 5,
     };
 
-    constexpr MediaTime();
-    constexpr MediaTime(int64_t value, uint32_t scale, uint8_t flags = Valid);
+    MediaTime();
+    MediaTime(int64_t value, uint32_t scale, uint8_t flags = Valid);
     MediaTime(const MediaTime& rhs);
 
     static MediaTime createWithFloat(float floatTime);
@@ -148,14 +148,14 @@ private:
     uint8_t m_timeFlags;
 };
 
-constexpr MediaTime::MediaTime()
+inline MediaTime::MediaTime()
     : m_timeValue(0)
     , m_timeScale(DefaultTimeScale)
     , m_timeFlags(Valid)
 {
 }
 
-constexpr MediaTime::MediaTime(int64_t value, uint32_t scale, uint8_t flags)
+inline MediaTime::MediaTime(int64_t value, uint32_t scale, uint8_t flags)
     : m_timeValue(value)
     , m_timeScale(scale)
     , m_timeFlags(flags)
